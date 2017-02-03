0ee85f0a51cda48e25e8a6ac13116d218733c32f
Alex Huynh <alex.huynh@mail.mcgill.ca>
2017-02-02 21:44:39 -0500

    C and ASM routine work, initial work of part 2

--- FIR_ASM.s
+++ FIR_ASM.s
@@ -1,4 +1,4 @@
-Area main, CODE, READONLY
+	AREA main, CODE, READONLY
 	EXPORT FIR_ASM
 FIR_ASM
 	;R0 is input x values
@@ -6,32 +6,32 @@ FIR_ASM
 	;R2 is b coefficients
 	;R3 length of input
 	;R4 is counter
-	ADD R4, R4, #4 ;adds 4 to couner to account for N-4
-	VLDR S5, [R2]
-	VLDR S6, [R2, #4]
-	VLDR S7, [R2, #8]
-	VLDR R3, [R3] ;take pointer and replace with value
-	loop
-	VLDR S8, [R0]; Takes x value and puts i register
-	ADD R3, R3, #4; Increments pointe to refer to next value
-	MUL S8, S5, S8; Multiplies coeff b0 and x
-	VSTR S9, [R0]
-	ADD R3, R3, #4; Refers to x + 2
-	MUL S9, S6, S9; multiples coeff b1 and x
-	ADD S8, S8, S9 ;Takes 2nd value and sums with first.
-	VLDR S9, [R0]
-	ADD R3, R3, #4; refers to x + 3
-	MUL S9, S7, S9; multiplies coeff b2 and x
-	ADD S8, S8, S9
-	VLDR S9, [R0]
-	ADD R3, R3, #4; refers to x + 4
-	MUL S9, S6, S9
-	ADD S8, S8, S9
-	VLDR S9, [R0]
-	ADD R3, R3, #4; refers to x start of next loop
-	MUL S9, S5, S9
-	ADD S8, S8, S9; adds the 5th value to the sum
-	VSTR R1, [R8]; Stores the ouput in the output array
+	;ADD R4, R4, #4 ;adds 4 to couner to account for N-4
+	VLDR.f32 S5, [R2]
+	VLDR.f32 S6, [R2, #4]
+	VLDR.f32 S7, [R2, #8]
+	;LDR R3, [R3] ;take pointer and replace with value
+loop
+	VLDR.f32 S8, [R0]; Takes x value and puts i register
+	ADD R0, R0, #4; Increments pointe to refer to next value
+	VMUL.f32 S8, S5, S8; Multiplies coeff b0 and x
+	VLDR.f32 S9, [R0]
+	ADD R0, R0, #4; Refers to x + 2
+	VMUL.f32 S9, S6, S9; multiples coeff b1 and x
+	VADD.f32 S8, S8, S9 ;Takes 2nd value and sums with first.
+	VLDR.f32 S9, [R0]
+	ADD R0, R0, #4; refers to x + 3
+	VMUL.f32 S9, S7, S9; multiplies coeff b2 and x
+	VADD.f32 S8, S8, S9
+	VLDR.f32 S9, [R0]
+	ADD R0, R0, #4; refers to x + 4
+	VMUL.f32 S9, S6, S9
+	VADD.f32 S8, S8, S9
+	VLDR.f32 S9, [R0]
+	ADD R0, R0, #4; refers to x start of next loop
+	VMUL.f32 S9, S5, S9
+	VADD.f32 S8, S8, S9; adds the 5th value to the sum
+	VSTR.f32 S8, [R1]; Stores the ouput in the output array
 	ADD R1, R1, #4
 	ADD R4, R4, #1 ;increments counter
 	ADD R0, R0, #-16
--- FIR_C.c
+++ FIR_C.c
@@ -1,4 +1,5 @@
 #include "FIR_C.h"
+#include <math.h>
 
 int FIR_C(float* InputArray, float* OutputArray, struct FIR_coeff* coeff, int Length, int Order) {
 	int input_array_pointer;
@@ -9,51 +10,29 @@ int FIR_C(float* InputArray, float* OutputArray, struct FIR_coeff* coeff, int Le
 	for(input_array_pointer = 0; input_array_pointer < Length; input_array_pointer++) {
 		sum = 0;
 		for(filter_coeff_pointer = 0; filter_coeff_pointer <= Order; filter_coeff_pointer++) {
-			switch(filter_coeff_pointer) {
-				case 0:
-					sum += coeff->b0 * InputArray[input_array_pointer - filter_coeff_pointer];
-					OutputArray[output_array_pointer] = sum;
-				
-				case 1:
-					if(input_array_pointer - filter_coeff_pointer >= 0) {
-						sum += coeff->b1 * InputArray[input_array_pointer - filter_coeff_pointer];
-					}
-					else {
-						OutputArray[output_array_pointer] = sum;
-						output_array_pointer++;
-						break;
-					}	
-				case 2:
-					if(input_array_pointer - filter_coeff_pointer >= 0) {
-						sum += coeff->b2 * InputArray[input_array_pointer - filter_coeff_pointer];
-					}
-					else {
-						OutputArray[output_array_pointer] = sum;
-						output_array_pointer++;
-						break;
-					}
-				case 3:
-					if(input_array_pointer - filter_coeff_pointer >= 0) {
-						sum += coeff->b3 * InputArray[input_array_pointer - filter_coeff_pointer];
-					}
-					else {
-						OutputArray[output_array_pointer] = sum;
-						output_array_pointer++;
-						break;
-					}
-				case 4:
-					if(input_array_pointer - filter_coeff_pointer >= 0) {
-						sum += coeff->b4 * InputArray[input_array_pointer - filter_coeff_pointer];
-					}
-					else {
-						OutputArray[output_array_pointer] = sum;
-						output_array_pointer++;
-						break;
-					}
+			if(filter_coeff_pointer == 0) {
+				sum = coeff->b0 * InputArray[input_array_pointer + 4 - filter_coeff_pointer];
+			}
+			else if(filter_coeff_pointer == 1) {
+				sum += coeff->b1 * InputArray[input_array_pointer + 4 - filter_coeff_pointer];
+			}
+			else if(filter_coeff_pointer == 2) {
+				sum += coeff->b2 * InputArray[input_array_pointer + 4 - filter_coeff_pointer];
+			}
+			else if(filter_coeff_pointer == 3) {
+				sum += coeff->b3 * InputArray[input_array_pointer + 4 - filter_coeff_pointer];
+			}
+			else{
+				sum += coeff->b4 * InputArray[input_array_pointer + 4 - filter_coeff_pointer];
 			}
 		}
+		
+		OutputArray[output_array_pointer] = sum;
+		output_array_pointer++;
 	}
 	
 	return 0;
 }
+
+
 	
